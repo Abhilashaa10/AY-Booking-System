dde7318b9b231344e3b4ebefd453f7a555a82f3b
Abhilasha sarode <sarodeabhilasha057@gmail.com>
2026-05-30 23:04:25 +0530

    pom files fixed

--- ticket-booking-system/notification-service/src/main/java/com/ticketing/notification/model/NotificationLog.java
+++ ticket-booking-system/notification-service/src/main/java/com/ticketing/notification/model/NotificationLog.java
@@ -1,5 +1,19 @@
-package com.ticketing.notification.model;
+```java id="l6hy2r"
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationLog {
-    
+
+    private String id;
+    private String bookingId;
+    private String email;
+    private String status;
+
 }
+```
